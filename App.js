4cb30a3304b41705215ae98a5165f346e385e333
ASABA DOREEN <doreenasaba187@gmail.com>
2022-11-28 08:47:40 +0300

    automatic pick up of coordinates

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
 export default function App() {
+  
+async function GetLocation(){
+  let { status } = await Location.requestForegroundPermissionsAsync()
+
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission not granted",
+        "Allow the app to use location service",
+        [{text: "OK"}],
+        {cancelable: false}
+      );
+    }
+  let coords = await Location.getCurrentPositionAsync({});
+
+  alert(JSON.stringify(coords))
+    }
+  
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Button title = "Show My Location" onPress={GetLocation}/>
       <StatusBar style="auto" />
     </View>
   );
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
